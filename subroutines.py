3cccbe8a15c4364aaa481465be867edda610cc24
Stewart Ravenhall <stewrav@gmail.com>
2015-09-28 19:59:14 +0100

    Added example subroutine

---
+++ subroutines.py
@@ -0,0 +1,9 @@
+# Define a subroutine called "hello"
+# that accepts an argument called "person"
+def hello(person):
+	print("Hello there", person, ", how are you today?")
+
+# Call the subroutine multiple times
+# with different arguments
+hello('John')
+hello('Zogga')
