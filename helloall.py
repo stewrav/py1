332765b39425ad0c6f646a51dc5512b26b06d987
puden <oliver.ravenhall@gmail.com>
2015-09-28 16:25:27 +0100

    made the code lok nicer

--- helloall.py
+++ helloall.py
@@ -1,6 +1,6 @@
 # Hello all
 
-everyone = ['mum', 'dad', 'boy', 'girl', 'dog', 'cat',"guinea pig","chicken"]
+everyone = ['mum', 'dad', 'boy', 'girl', 'dog', 'cat', 'guinea pig', 'chicken']
 
 for familymember in everyone:
     print('Hello', familymember)
