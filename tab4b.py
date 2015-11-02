f1b9b916bcc82b124c7b2c0f448bc726bbd72a4d
Oliver Ravenhall <Oliver Ravenhall>
2015-11-02 14:45:20 +0000

    added timer

--- tab4b.py
+++ tab4b.py
@@ -2,6 +2,7 @@
 
 from random import randint
 
+import time
 
 tables = [2,3,6]
 tables2 = range(1,13)
@@ -9,6 +10,11 @@ tables2 = range(1,13)
 numcor = 0
 
 nq=5
+
+start_time = time.monotonic()
+
+
+
 
 for question_num in range (0,nq):
 
@@ -30,8 +36,11 @@ for question_num in range (0,nq):
 	else:
 		print("you were wrong! The answer was",x*y)
 	
+end_time = time.monotonic()
 
-print ("you got",numcor,"out of",nq)
+time_taken = int(0.5 + end_time - start_time)
+	
+print ("you got",numcor,"out of",nq, "You did it in", time_taken,"seconds!")
 
 if numcor == nq:
 	print ("You got all the questions correct! Well done!")
