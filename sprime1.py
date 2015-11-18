a0a41c910dcd60ea1eca4cc232bcdfce9582d353
Stewart Ravenhall <stewrav@gmail.com>
2015-11-18 22:57:34 +0000

    Added comments

--- sprime1.py
+++ sprime1.py
@@ -1,8 +1,18 @@
 # Prime numbers
+
+# Test these numbers
 for i in range(2,100):
+
+	# Test factors
 	for j in range(2,i):
+
+		# If divided with no remainder
 		if i % j == 0:
+
+			# then skip to next number
 			break
+	# If got to here then did not break
 	else:
+		# So we had no factors
 		print(i)
 
