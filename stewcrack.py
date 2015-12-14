6406ed00dc181a07b54fbbb30e151282bd5b88f0
Stewart Ravenhall <stewrav@gmail.com>
2015-12-14 20:19:58 +0000

    Checkpoint

--- stewcrack.py
+++ stewcrack.py
@@ -11,9 +11,10 @@ for guess in ascii_lowercase:
 	encoded_guess = guess.encode('utf-8')
 	hashed = hashlib.md5(encoded_guess).hexdigest()
 	if( hashed == answer ):
-		print("password is", guess)
+		print("password 1 is", guess)
 		break
 
+#------------------------------------------------
 
 # Could be one or two letters or symbols
 answer = "e22428ccf96cda9674a939c209ad1000"
@@ -22,7 +23,7 @@ for guess in printable:
 	encoded_guess = guess.encode('utf-8')
 	hashed = hashlib.md5(encoded_guess).hexdigest()
 	if( hashed == answer ):
-		print("password is", guess)
+		print("password 2 is", guess)
 		break
 
 for g0 in printable:
@@ -33,3 +34,14 @@ for g0 in printable:
 		if( hashed == answer ):
 			print("password is", guess)
 			break
+
+#------------------------------------------------
+
+# Could be up to eight letters or symbols
+answer="fc45ed5aa04ed400b192101a11f5e580"
+
+max_guess_length = 8
+guess_length = 1
+
+while( guess_length <= max_guess_length ):
+	
