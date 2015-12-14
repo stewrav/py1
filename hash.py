d99c1924330e583f03f077ac6ebc70fccead4076
Stewart Ravenhall <stewart.ravenhall@roche.com>
2015-12-14 18:02:14 +0100

    Updated with 2 character password cracker

--- hash.py
+++ hash.py
@@ -1,8 +1,11 @@
 import hashlib
 
-secret = "open sesame"
-encoded_secret = secret.encode('utf-8')
-hashed = hashlib.md5(encoded_secret).hexdigest()
+def show_pw_and_hash(secret):
+	print("secret ==", secret)
+	encoded_secret = secret.encode('utf-8')
+	hashed = hashlib.md5(encoded_secret).hexdigest()
+	print("hashed ==", hashed)
 
-print("secret ==", secret)
-print("hashed ==", hashed)
+show_pw_and_hash("open sesame")
+show_pw_and_hash("t")
+show_pw_and_hash("sr")
--- stewcrack.py
+++ stewcrack.py
@@ -2,12 +2,34 @@
 # assume MD5 sum
 
 import hashlib
-from string import ascii_lowercase
+from string import ascii_lowercase, printable
 
+# One lowercase letter only
 answer = "e358efa489f58062f10dd7316b65649e"
 
 for guess in ascii_lowercase:
 	encoded_guess = guess.encode('utf-8')
 	hashed = hashlib.md5(encoded_guess).hexdigest()
-	if( hashed == answer):
+	if( hashed == answer ):
 		print("password is", guess)
+		break
+
+
+# Could be one or two letters or symbols
+answer = "e22428ccf96cda9674a939c209ad1000"
+
+for guess in printable:
+	encoded_guess = guess.encode('utf-8')
+	hashed = hashlib.md5(encoded_guess).hexdigest()
+	if( hashed == answer ):
+		print("password is", guess)
+		break
+
+for g0 in printable:
+	for g1 in printable:
+		guess = g0 + g1
+		encoded_guess = guess.encode('utf-8')
+		hashed = hashlib.md5(encoded_guess).hexdigest()
+		if( hashed == answer ):
+			print("password is", guess)
+			break
