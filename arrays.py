c4b01417ec13d56223b348ec126bdf8a2b4b1dd7
Stewart Ravenhall <stewart.ravenhall@roche.com>
2015-12-16 10:50:27 +0100

    Added arrays.py

---
+++ arrays.py
@@ -0,0 +1,14 @@
+a = ['one','two','three']
+a.append('four')
+
+print("All elements:")
+for i in a:
+	print("\t",i)
+print()
+
+print("Second element:")
+print("\t",a[1])
+print()
+
+print("Last element:")
+print("\t",a[len(a)-1])
