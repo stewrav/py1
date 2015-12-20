fcdfeef7565d88e3a047ecf38aadfed1ca85a502
Stewart Ravenhall <stewart.ravenhall@roche.com>
2015-12-20 16:47:39 +0100

    checkpoint: updated stewcrack

--- stewcrack.py
+++ stewcrack.py
@@ -17,14 +17,17 @@ for guess in ascii_lowercase:
 #------------------------------------------------
 
 # Could be one or two letters or symbols
+
+def find_pw( numchars ):
+	
+
+#sr
 answer = "e22428ccf96cda9674a939c209ad1000"
 
-for guess in printable:
-	encoded_guess = guess.encode('utf-8')
-	hashed = hashlib.md5(encoded_guess).hexdigest()
-	if( hashed == answer ):
-		print("password 2 is", guess)
-		break
+#t
+answer = "e358efa489f58062f10dd7316b65649e"
+
+pw = find_pw(1) or find_pw(2)
 
 for g0 in printable:
 	for g1 in printable:
@@ -32,7 +35,7 @@ for g0 in printable:
 		encoded_guess = guess.encode('utf-8')
 		hashed = hashlib.md5(encoded_guess).hexdigest()
 		if( hashed == answer ):
-			print("password is", guess)
+			print("password 2 is", guess)
 			break
 
 #------------------------------------------------
@@ -40,8 +43,8 @@ for g0 in printable:
 # Could be up to eight letters or symbols
 answer="fc45ed5aa04ed400b192101a11f5e580"
 
-max_guess_length = 8
-guess_length = 1
-
-while( guess_length <= max_guess_length ):
-	
+#for guess in printable:
+#	encoded_guess = guess.encode('utf-8')
+#	hashed = haslib.md5(encoded_guess).hexdigest()
+#	if( hashed == answer ):
+#		print(
