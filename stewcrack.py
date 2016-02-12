83bcc7800e33322cd26aadd03e71ebc67a0e0700
Stewart Ravenhall <stewart.ravenhall@roche.com>
2016-02-12 11:08:37 +0100

    Finished stewcrack

--- stewcrack.py
+++ stewcrack.py
@@ -21,27 +21,93 @@ for guess in printable:
 #sr
 answer = "e22428ccf96cda9674a939c209ad1000"
 
-#t
-#answer = "e358efa489f58062f10dd7316b65649e"
+def guess_correct(g):
+	encoded_guess = g.encode('utf-8')
+	hashed = hashlib.md5(encoded_guess).hexdigest()
+	if( hashed == answer ):
+		print("password is", g)
+		return True
+	else:
+		return False
 
-for g0 in printable:
-	for g1 in printable:
-		guess = g0 + g1
-		encoded_guess = guess.encode('utf-8')
-		hashed = hashlib.md5(encoded_guess).hexdigest()
-		if( hashed == answer ):
-			print("password 2 is", guess)
+for g1 in printable:
+	for g2 in printable:
+		if guess_correct(g1 + g2):
 			break
 
 #------------------------------------------------
-"""
+
+#"""
 
 # Could be up to eight letters or symbols
 answer="fc45ed5aa04ed400b192101a11f5e580"
 
-#for guess in printable:
-#	encoded_guess = guess.encode('utf-8')
-#	hashed = haslib.md5(encoded_guess).hexdigest()
-#	if( hashed == answer ):
-#		print(
-"""
+# 1
+for g1 in printable:
+	if guess_correct(g1):
+		quit()
+
+# 2
+for g1 in printable:
+	for g2 in printable:
+		if guess_correct(g1+g2):
+			quit()
+
+# 3
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			if guess_correct(g1+g2+g3):
+				quit()
+
+# 4
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			for g4 in printable:
+				if guess_correct(g1+g2+g3+g4):
+					quit()
+
+# 5
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			for g4 in printable:
+				for g5 in printable:
+					if guess_correct(g1+g2+g3+g4+g5):
+						quit()
+
+# 6
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			for g4 in printable:
+				for g5 in printable:
+					for g6 in printable:
+						if guess_correct(g1+g2+g3+g4+g5+g6):
+							quit()
+
+# 7
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			for g4 in printable:
+				for g5 in printable:
+					for g6 in printable:
+						for g7 in printable:
+							if guess_correct(g1+g2+g3+g4+g5+g6+g7):
+								quit()
+
+#8
+for g1 in printable:
+	for g2 in printable:
+		for g3 in printable:
+			for g4 in printable:
+				for g5 in printable:
+					for g6 in printable:
+						for g7 in printable:
+							for g8 in printable:
+								if guess_correct(g1+g2+g3+g4+g5+g6+g7+g8):
+									quit()
+
+#"""
